8fba99d14f052f6fc4063fd5bb5a27cc1c4d797a
Gurpreeet <n01570640@humber.ca>
2026-03-04 11:12:36 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YogaStudioLRAManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
