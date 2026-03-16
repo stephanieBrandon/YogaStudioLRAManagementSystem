1ec0da9a1b33d95c032dc15f508b6e683497bd00
Gurpreeet <n01570640@humber.ca>
2026-03-16 19:48:57 -0400

    Create the User and Attendance Models - still waiting to add properties that depend on the other models

---
+++ Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YogaStudioLRAManagementSystem.Models
+{
+    [Table("USERS")]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //to ensure db generates the value
+        public int UserId { get; set; }
+
+        [Column("user_name")]
+        [Required]
+        [StringLength(50,MinimumLength = 3, ErrorMessage ="Username is required.")]
+        public string UserName { get; set; }
+
+        [Column("password")]
+        [Required]
+        public string Password { get; set; }
+
+        [Column("email_id")]
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Column("profile_role")]
+        [Required]
+        public string Role { get; set; }
+
+        //EmployeeId (FK) - multitable 
+        //waiting on getting the code from Katherine
+
+
+    }
+}
