265b1fc9b73a1dfb387606b3a920677cb7cd73e4
user.name <katherine.dorensky@gmail.com>
2026-03-27 18:59:14 -0400

    Tweaked leave type and leave request to better match proposal

--- Models/LeaveRequest.cs
+++ Models/LeaveRequest.cs
@@ -13,31 +13,35 @@ namespace YogaStudioLRAManagementSystem.Models
 
         [Required]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")] //what date format do we prefer?
-        public DateTime StartDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")] 
+        public DateOnly StartDate { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-        //How to set it 'less than' the start date?
-        public DateTime EndDate { get; set; }
+        public DateOnly EndDate { get; set; }
 
         [Required]
         [StringLength(6, ErrorMessage = "Status is required")]
-        //Possible Values: ACTIVE || CLOSED
-        public string Status {  get; set; } //this would be selected via drop down (?),
-        public List<string> StatusOptions { get; set; } = new List<string> {"Active", "Closed"};
+
+        //Possible Values: "Absent", "Present", "On Leave"
+        public string Status {  get; set; } //this would be selected via drop down
+        public List<string> StatusOptions { get; set; } = new List<string> {};
 
         [StringLength(150, MinimumLength = 2)]
-        public string? Reason { get; set; } //I don't think you should be saying always why you want an off-day?
+        public string? Reason { get; set; }
 
         //Foreign keys:
-
+        [Required]
+        public int EmployeeId { get; set; }
         [ForeignKey("EmployeeId")]
 
+        public Employee Employee { get; set; }
+
+        [Required]
+        public int LeaveTypeId { get; set; }
         [ForeignKey("LeaveTypeId")]
 
-
-
+        public LeaveType LeaveType { get; set; }
     }
 }
