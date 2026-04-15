5d6fc0187f1af255dfb79790fed3f567dbe37b5d
user.name <katherine.dorensky@gmail.com>
2026-04-15 17:22:30 -0400

    # Created Attendance Controller using EF - doesn't having anything working, just pushing to have a template ready

---
+++ Controllers/AttendanceController.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using YogaStudioLRAManagementSystem.Data;
+using YogaStudioLRAManagementSystem.Models;
+
+namespace YogaStudioLRAManagementSystem.Controllers
+{
+    /*
+       - Clock in, Clock Out 
+       - view personal time tracking history
+       - manager report filtered by employee, date range and status
+       - View attendance records and reports 
+    */
+    [Authorize] //must be already authenticated
+    public class AttendanceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendanceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TimeTrackingAttendance
+        public async Task<IActionResult> Index() //this should display personal time tracking history + manager report view combined
+        {
+            var applicationDbContext = _context.Attendances.Include(a => a.Employee);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: TimeTrackingAttendance/Details/5
+        public async Task<IActionResult> Details(int? id) //display a specific record
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // GET: TimeTrackingAttendance/Create
+        public IActionResult Create() 
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "FirstName");
+            return View();
+        }
+
+        // POST: TimeTrackingAttendance/Create
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AttendanceId,EmployeeId,Date,ClockIn,ClockOut,TotalHours,Status")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(attendance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "FirstName", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        // GET: TimeTrackingAttendance/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "FirstName", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        // POST: TimeTrackingAttendance/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AttendanceId,EmployeeId,Date,ClockIn,ClockOut,TotalHours,Status")] Attendance attendance)
+        {
+            if (id != attendance.AttendanceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(attendance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AttendanceExists(attendance.AttendanceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "FirstName", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        // GET: TimeTrackingAttendance/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // POST: TimeTrackingAttendance/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance != null)
+            {
+                _context.Attendances.Remove(attendance);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AttendanceExists(int id)
+        {
+            return _context.Attendances.Any(e => e.AttendanceId == id);
+        }
+    }
+}
